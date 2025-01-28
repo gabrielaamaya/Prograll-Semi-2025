0d858194f10a3573575b90427039aad2f7a22a87
gabrielaamaya <amayagabriela073@gmail.com>
2025-01-27 19:55:25 -0600

    Creacion de la Calculadora de suma

--- app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myprimeraaplicacion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +11,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myprimeraaplicacion.R;
 
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView tempVal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempVal = findViewById(R.id.txtNum1);
+                double num1 = Double.parseDouble(tempVal.getText().toString());
+
+                tempVal= findViewById(R.id.txtNum2);
+                double num2 = Double.parseDouble(tempVal.getText().toString());
+
+                double respuesta = num1 + num2;
+
+                tempVal= findViewById(R.id.lblRespuesta);
+                tempVal.setText("Respuesta: "+ respuesta);
+            }
+        });
+
     }
 }
