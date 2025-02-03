29cec234539fa2bf92ee5dd47c8ff125a9788715
gabrielaamaya <amayagabriela073@gmail.com>
2025-02-03 12:12:34 -0600

    Uso del spinner en la calculadora

--- app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,63 +22,40 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
-    RadioGroup rgo;
-    RadioButton opt;
-
+    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btn = findViewById(R.id.btnCalcular);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tempVal = findViewById(R.id.txtNum1);
                 double num1 = Double.parseDouble(tempVal.getText().toString());
-
-                tempVal= findViewById(R.id.txtNum2);
+                tempVal = findViewById(R.id.txtNum2);
                 double num2 = Double.parseDouble(tempVal.getText().toString());
 
                 double respuesta = 0.0;
 
-                opt = findViewById(R.id.optSuma);
-                if (opt.isChecked()) {
-                    respuesta = num1 + num2;
+                spn = findViewById(R.id.spnOpciones);
+                switch (spn.getSelectedItemPosition()){
+                    case 0:
+                        respuesta = num1 + num2;
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        break;
+                    case 3:
+                        respuesta = num1 / num2;
+                        break;
                 }
-                opt = findViewById(R.id.optResta);
-                if (opt.isChecked()) {
-                    respuesta = num1 - num2;
-                }
-                opt = findViewById(R.id.optMultiplicacion);
-                if (opt.isChecked()) {
-                    respuesta = num1 * num2;
-                }
-                opt = findViewById(R.id.optDivision);
-                if (opt.isChecked()) {
-                    respuesta = num1 / num2;
-                }
-                opt = findViewById(R.id.optExponenciacion);
-                if (opt.isChecked()) {
-                    respuesta = Math.pow(num1, num2);
-                }
-                opt = findViewById(R.id.optPorcentaje);
-                if (opt.isChecked()) {
-                    respuesta = (num1 * num2) / 100;
-                }
-                opt = findViewById(R.id.optRaiz);
-                if (opt.isChecked()) {
-                    if (num1 >= 0);
-                }
-                opt = findViewById(R.id.optFactorial);
-                if (opt.isChecked()) {
-                    if (num1 >= 0 && num1 == (int) num1);
-                }
-
-                tempVal= findViewById(R.id.lblRespuesta);
+                tempVal = findViewById(R.id.lblRespuesta);
                 tempVal.setText("Respuesta: "+ respuesta);
             }
         });
-
     }
 }
