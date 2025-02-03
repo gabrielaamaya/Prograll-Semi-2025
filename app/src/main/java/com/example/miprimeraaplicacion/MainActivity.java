79f20398be69491c128f5536d9e64820313b01db
Jennifer_baires <jennferbaires@gmail.com>
2025-02-03 13:24:46 -0600

    Uso de pestañas (Tabhost) y conversores

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,49 +18,54 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tbh;
     Button btn;
     TextView tempVal;
     Spinner spn;
+    conversores objConversores = new conversores();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tbh = findViewById(R.id.tbhConversor);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("Tiempo").setContent(R.id.tabTiempo).setIndicator("TIEMPO", null));
+        tbh.addTab(tbh.newTabSpec("Almacenamiento").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+
         btn = findViewById(R.id.btnCalcular);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
+                int opcion = tbh.getCurrentTab();
 
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
+                spn = findViewById(R.id.spnDeMonedas);
+                int de = spn.getSelectedItemPosition();
 
-                double respuesta = 0.0;
-                String msg = "";
-                spn = findViewById(R.id.spnOpciones);
-                switch (spn.getSelectedItemPosition()){
-                    case 0:
-                        respuesta = num1 + num2;
-                        msg = "La suma es: "+ respuesta;
-                        break;
-                    case 1:
-                        respuesta = num1 - num2;
-                        msg = "La resta es: "+ respuesta;
-                        break;
-                    case 2:
-                        respuesta = num1 * num2;
-                        msg = "La multiplicacion es: "+ respuesta;
-                        break;
-                    case 3:
-                        respuesta = num1 / num2;
-                        msg = "La division: "+ respuesta;
-                        break;
-                }
+                spn = findViewById(R.id.spnAMonedas);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtCantidad);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
                 tempVal = findViewById(R.id.lblRespuesta);
+                double respuesta = objConversores.convertir(opcion, de, a, cantidad);
                 tempVal.setText("Respuesta: "+ respuesta);
-                Toast.makeText(MainActivity.this, "msg", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+}
+class conversores{
+    double[][] valores= {
+            {1,0.98, 7.73, 25.45, 36.78, 508.87, 8.74},//monedas
+            {},//Longitud
+            {},//tiempo
+            {},//Almacenamiento
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
