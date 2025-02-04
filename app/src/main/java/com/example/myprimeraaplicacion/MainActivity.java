6daf68ccb1f001612d9cf6f52d97ef90efcdf0e6
gabrielaamaya <amayagabriela073@gmail.com>
2025-02-04 09:07:54 -0600

    Tarea

--- app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -21,55 +22,119 @@ import com.example.myprimeraaplicacion.R;
 import org.w3c.dom.Text;
 
 
+
 public class MainActivity extends AppCompatActivity {
-    TabHost tbh;
     Button btn;
-    TextView tempVal;
+    TextView lblRespuesta;
+    EditText txtNum1, txtNum2;
     Spinner spn;
-    conversores objConversores = new conversores();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tbh = findViewById(R.id.tbhConversor);
-        tbh.setup();
-
-        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
-        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
-        tbh.addTab(tbh.newTabSpec("Tiempo").setContent(R.id.tabTiempo).setIndicator("TIEMPO", null));
-        tbh.addTab(tbh.newTabSpec("Almacenamiento").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
-
         btn = findViewById(R.id.btnCalcular);
+        txtNum1 = findViewById(R.id.txtNum1);
+        txtNum2 = findViewById(R.id.txtNum2);
+        lblRespuesta = findViewById(R.id.lblRespuesta);
+        spn = findViewById(R.id.spnOpciones);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int opcion = tbh.getCurrentTab();
+                if (txtNum1.getText().toString().isEmpty()) {
+                    txtNum1.setError("Ingrese un número");
+                    return;
+                }
 
-                spn = findViewById(R.id.spnDeMonedas);
-                int de = spn.getSelectedItemPosition();
+                double num1 = Double.parseDouble(txtNum1.getText().toString());
+                double num2 = 0;
+                if (!txtNum2.getText().toString().isEmpty()) {
+                    num2 = Double.parseDouble(txtNum2.getText().toString());
+                }
 
-                spn = findViewById(R.id.spnAMonedas);
-                int a = spn.getSelectedItemPosition();
+                double respuesta = 0.0;
+                String msg = "";
 
-                tempVal = findViewById(R.id.txtCantidad);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
+                switch (spn.getSelectedItemPosition()) {
+                    case 0:
+                        respuesta = num1 + num2;
+                        msg = "La suma es: " + respuesta;
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        msg = "La resta es: " + respuesta;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        msg = "La multiplicación es: " + respuesta;
+                        break;
+                    case 3:
+                        if (num2 == 0) {
+                            msg = "Error: División por cero";
+                            respuesta = Double.NaN;
+                        } else {
+                            respuesta = num1 / num2;
+                            msg = "La división es: " + respuesta;
+                        }
+                        break;
+                    case 4:
+                        respuesta = Math.pow(num1, num2);
+                        msg = "La exponenciación es: " + respuesta;
+                        break;
+                    case 5:
+                        respuesta = (num1 * num2) / 100;
+                        msg = "El porcentaje es: " + respuesta;
+                        break;
+                    case 6:
+                        respuesta = Math.sqrt(num1);
+                        msg = "La raíz cuadrada es: " + respuesta;
+                        break;
+                    case 7:
+                        respuesta = Math.cbrt(num1);
+                        msg = "La raíz cúbica es: " + respuesta;
+                        break;
+                    case 8:
+                        if (num1 < 0 && num2 % 2 == 0) {
+                            msg = "Error: No se puede calcular raíz de índice par de un número negativo";
+                            respuesta = Double.NaN;
+                        } else {
+                            respuesta = Math.pow(num1, 1.0 / num2);
+                            msg = "La raíz de índice " + num2 + " es: " + respuesta;
+                        }
+                        break;
+                    case 9:
+                        if (num1 < 0 || num1 != (int) num1) {
+                            msg = "Error: El factorial solo se define para enteros positivos";
+                            respuesta = Double.NaN;
+                        } else {
+                            respuesta = factorial((int) num1);
+                            msg = "El factorial es: " + respuesta;
+                        }
+                        break;
+                    case 10:
+                        respuesta = num1 % num2;
+                        msg = "El módulo es: " + respuesta;
+                        break;
+                    case 11:
+                        respuesta = Math.max(num1, num2);
+                        msg = "El mayor número es: " + respuesta;
+                        break;
+                }
 
-                tempVal = findViewById(R.id.lblRespuesta);
-                double respuesta = objConversores.convertir(opcion, de, a, cantidad);
-                tempVal.setText("Respuesta: "+ respuesta);
+                lblRespuesta.setText("Respuesta: " + (Double.isNaN(respuesta) ? "Error" : respuesta));
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
             }
         });
     }
-}
-class conversores{
-    double[][] valores= {
-            {1,0.98, 7.73, 25.45, 36.78, 508.87, 8.74},//monedas
-            {},//Longitud
-            {},//tiempo
-            {},//Almacenamiento
-    };
-    public double convertir(int opcion, int de, int a, double cantidad){
-        return valores[opcion][a] / valores[opcion][de] * cantidad;
+
+    private int factorial(int n) {
+        if (n == 0 || n == 1) return 1;
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
