abe95bbec5125a793d0a615077c331cfc81c5488
gabrielaamaya <amayagabriela073@gmail.com>
2025-02-11 17:33:48 -0600

    Funcionamiento del sensor de luz

--- app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.myprimeraaplicacion;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,13 +30,64 @@ import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
+   TextView temVal;
+   SensorManager sensorManager;
+   Sensor sensor;
+   SensorEventListener sensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sensorLuz();
+    }
 
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    private void  iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+    private void sensorLuz(){
+        temVal = findViewById(R.id.lblSensorLuz);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(sensor==null ){
+            temVal.setText("Tu dispositivo, NO tiene el sensor de Luz");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                double valor = event.values[0];
+                temVal.setText("Cantidad de Luz: "+ valor);
+
+                if(valor<=20){
+                    getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+                }else if(valor<=50){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                }else {
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
     }
 }
 
