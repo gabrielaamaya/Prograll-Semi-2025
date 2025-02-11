9e34a542f118facfda3561b4baf05dc676204dd1
gabrielaamaya <amayagabriela073@gmail.com>
2025-02-11 16:07:24 -0600

    Creacion de la rama sensor de luz

--- app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/myprimeraaplicacion/MainActivity.java
@@ -26,6 +26,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
