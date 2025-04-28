57a316907c3a2f814e3ebc257547ab997004b17d
gabrielaamaya <amayagabriela073@gmail.com>
2025-04-28 17:52:37 +0000

    Iniciamos la configuración de las notificaciones push con firebasa,completada

--- app/src/main/java/com/example/myprimeraaplicacion/utilidades.java
+++ app/src/main/java/com/example/myprimeraaplicacion/utilidades.java
@@ -4,7 +4,7 @@ import java.util.Base64;
 
 public class utilidades {
     static String url_consulta = "http://:   192.168.1.10:5984/agenda/_desing/agenda/_view/agenda";
-    static String url_mto = "http://:   192.168.1.10:5984/agenda";
+    static String url_mto = "http://:    192.168.1.10:5984/agenda";
     static String user = "gabriela";
     static String passwd = "1234";
     static String credencialesCodificadas = Base64.getEncoder().encodeToString((user + ":"+passwd).getBytes());
