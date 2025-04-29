a2362cc848451a5ffe32c73d6fe78d4adabf7a82
josue chicas <josue.a.mejia@hotmail.com>
2025-04-29 09:47:53 -0600

    parcial terminado

--- app/src/main/java/com/example/miprimeraaplicacion/utilidades.java
+++ app/src/main/java/com/example/miprimeraaplicacion/utilidades.java
@@ -8,8 +8,8 @@ import java.util.Base64;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class utilidades {
-    static String url_consulta = "http://: 192.168.80.57:5984/josue/_design/_design%2Fjosue/_view/Gabriela";
-    static String url_mto = "http://: 192.168.80.57:5984/josue";
+    static String url_consulta = "http://: 192.168.81.1:5984/josue/_design/_design%2Fjosue/_view/Gabriela";
+    static String url_mto = "http://: 192.168.81.1:5984/josue";
     static String user = "josuealejandro";//Agregar usuario
     static String passwd = "123456789";//Agregar contraseña
     static String credencialesCodificadas = Base64.getEncoder().encodeToString((user + ":" + passwd).getBytes());
