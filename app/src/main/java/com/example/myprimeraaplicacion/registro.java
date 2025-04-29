a33beadf2a1ed4498d14d2bc474f2fa58aab7230
gabrielaamaya <amayagabriela073@gmail.com>
2025-04-29 10:29:27 -0600

    creacio de lapantalla de registro de usuario

---
+++ app/src/main/java/com/example/myprimeraaplicacion/registro.java
@@ -0,0 +1,16 @@
+package com.example.myprimeraaplicacion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class registro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+    }
+}
